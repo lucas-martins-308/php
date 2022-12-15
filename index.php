f3ea79f275a625f281c9a5004885b03de7ce0388
Lucas Martins <lucasmartins308@gmail.com>
2022-12-15 11:52:55 -0300

    Teste

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 
      echo "Olá Mundo"
      
-     echo "Inicializando Matrix"
+     echo (Inicializando Matrix)
 
-     echo "Olá Mundo!"
+     echo 'Olá Mundo!'
 
      echo "Carregando emuladores de Nintendo 64"
 ?>
