f1eba9d01a3d680230d11aca7918e86c178cf4bb
Lucas Martins <lucasmartins308@gmail.com>
2022-12-15 11:47:42 -0300

    Correção

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 
-<<<<<<< HEAD
+
      echo "Olá Mundo"
      
      echo "Inicializando Matrix"
-=======
+
      echo "Olá Mundo!"
->>>>>>> cb000e2c1d0a5cdfa9c6e6d2e2b6c7cc31038d6c
 
      echo "Carregando emuladores de Nintendo 64"
 ?>
